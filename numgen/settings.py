7430b44ee52373ca07009367f6a716d2054f637f
Adesina Joshua <tolujosh1@gmail.com>
2024-11-13 11:11:07 +0100

    final

--- numgen/settings.py
+++ numgen/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 

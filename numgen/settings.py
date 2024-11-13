09900eff69b4bcf12566c84285b1c9ffeb46a50a
Adesina Joshua <tolujosh1@gmail.com>
2024-11-13 10:10:00 +0100

    final

--- numgen/settings.py
+++ numgen/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
 
 # Default primary key field type
